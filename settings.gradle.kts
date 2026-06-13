4f908795f259618299c919eff0eae77dcfcf6ad1
Neo <81220275+NeoNexus402@users.noreply.github.com>
2026-06-13 16:49:03 +0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -33,10 +33,11 @@ include(":discordrpc")
 include(":lastfm")
 include(":betterlyrics")
 include(":shazamkit")
-include(":paxsenix")
+include(":paxsenix")
+
 
 // Use a local copy of NewPipe Extractor by uncommenting the lines below.
-// We assume, that Metrolist and NewPipe Extractor have the same parent directory.
+// We assume, that Loiter and NewPipe Extractor have the same parent directory.
 // If this is not the case, please change the path in includeBuild().
 //
 // For this to work you also need to change the implementation in innertube/build.gradle.kts
